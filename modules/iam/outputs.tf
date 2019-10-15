1617bb4519dfeede187807e612a938a726b0a106
Pratiksha96 <jainpratiksha2110@gmail.com>
2019-10-15 14:12:05 +0530

    Added description to outputs.

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,13 +1,16 @@
 output "service-account-email" {
   value = ["${google_service_account.service_account.*.email}"]
-  description = "Service account email"
+  description = "Email of service account created."
 }
 output "iam-binding-roles" {
   value = ["${google_project_iam_binding.iam_binding.*.role}"]
+  description = "Roles added to the project."
 }
 output "iam-binding-members" {
   value = ["${google_project_iam_binding.iam_binding.*.members}"]
+  description = "IAM Binding added on the members to the project."
 }
 output "iam-custom-role" {
   value = ["${google_project_iam_custom_role.custom_role.*.role_id}"]
+  description = "Custom roles created in a project."
 }
--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,19 @@
 output "service-account-email" {
-  value = "${module.iam-management.service-account-email}"
+  value = module.iam-management.service-account-email
+  description = "Email of service account created."
 }
+
 output "iam-binding-roles" {
-  value = "${module.iam-management.iam-binding-roles}"
+  value = module.iam-management.iam-binding-roles
+  description = "Roles added to the project."
 }
+
 output "iam-binding-members" {
-  value = "${module.iam-management.iam-binding-members}"
+  value = module.iam-management.iam-binding-members
+  description = "IAM Binding added on the members to the project."
 }
+
 output "iam-custom-role" {
-  value = "${module.iam-management.iam-custom-role}"
+  value = module.iam-management.iam-custom-role
+  description = "Custom roles created in a project."
 }
