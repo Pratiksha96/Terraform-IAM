c17ff661f2dc9211f1dfa209b359e7fc94c1694e
Pratiksha96 <jainpratiksha2110@gmail.com>
2019-10-15 11:36:37 +0530

    Added outputs.tf.

--- iam/main.tf
+++ iam/main.tf
@@ -27,15 +27,3 @@ resource "google_project_iam_binding" "self" {
     google_service_account.self
   ]
 }
-output "service-account-email" {
-  value = "${google_service_account.self.*.email}"
-}
-output "iam-binding-roles" {
-  value = "${google_project_iam_binding.self.*.role}"
-}
-output "iam-binding-members" {
-  value = "${google_project_iam_binding.self.*.members}"
-}
-output "iam-custom-role" {
-  value = "${google_project_iam_custom_role.self.*.role_id}"
-}
---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "service-account-email" {
+  value = "module.iam-management.service-account-email"
+}
+output "iam-binding-roles" {
+  value = "module.iam-management.iam-binding-roles"
+}
+output "iam-binding-members" {
+  value = "module.iam-management.iam-binding-members"
+}
+output "iam-custom-role" {
+  value = "module.iam-management.iam-custom-role"
+}
